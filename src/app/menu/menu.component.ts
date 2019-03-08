f6985e3234372c80744a6d7fd333565cc173300c
Bhargavi Patel <bhargavipatel259@gmail.com>
2019-03-08 18:15:09 -0500

    component part 1

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { Project } from '../shared/project';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+
+  projects: Project[] = [
+    {
+      id: '0',
+      name: 'UAlbany Hangout',
+      image: '/assets/images/bg.png',
+      category: 'Academic Project',
+      featured: true,
+      label: ' ',
+      // tslint:disable-next-line:max-line-length
+      description: ' '
+    },
+    {
+      id: '1',
+      name: 'Miles',
+      image: '/assets/images/Picture1.png',
+      category: 'Academic Project',
+      featured: false,
+      label: '',
+      description: ' '
+    },
+    {
+      id: '2',
+      name: 'Face Recognition',
+      image: '/assets/images/facial-recognition.jpg',
+      category: 'Academic Project',
+      featured: false,
+      label: ' ',
+      description: ' '
+    },
+    {
+      id: '3',
+      name: 'Portfolio Website',
+      image: '/assets/images/123.jpg',
+      category: 'Academic Project',
+      featured: false,
+      label: '',
+      description: ' '
+    }
+   ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/shared/project.ts
@@ -0,0 +1,9 @@
+export class Project {
+    id: string;
+    name: string;
+    image: string;
+    category: string;
+    featured: boolean;
+    label: string;
+    description: string;
+}
