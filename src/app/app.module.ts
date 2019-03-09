2fd08ddd6411dd565163da04df6304ed2c4d8086
Bhargavi Patel <bhargavipatel259@gmail.com>
2019-03-08 20:12:35 -0500

    components part2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { MatListModule } from '@angular/material/list';
 
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
@@ -20,7 +24,10 @@ import { MatListModule } from '@angular/material/list';
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { Project } from '../shared/project';
 
+
+const PROJECTS: Project[] = [
+  {
+    id: '0',
+    name: 'UAlbany Hangout',
+    image: '/assets/images/bg.png',
+    category: 'Academic Project',
+    featured: true,
+    label: ' ',
+    // tslint:disable-next-line:max-line-length
+    description: ' '
+  },
+  {
+    id: '1',
+    name: 'Miles',
+    image: '/assets/images/Picture1.png',
+    category: 'Academic Project',
+    featured: false,
+    label: '',
+    description: ' '
+  },
+  {
+    id: '2',
+    name: 'Face Recognition',
+    image: '/assets/images/facial-recognition.jpg',
+    category: 'Academic Project',
+    featured: false,
+    label: ' ',
+    description: ' '
+  },
+  {
+    id: '3',
+    name: 'Portfolio Website',
+    image: '/assets/images/123.jpg',
+    category: 'Academic Project',
+    featured: false,
+    label: '',
+    description: ' '
+  }
+ ];
+
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -8,46 +50,10 @@ import { Project } from '../shared/project';
 })
 export class MenuComponent implements OnInit {
 
-  projects: Project[] = [
-    {
-      id: '0',
-      name: 'UAlbany Hangout',
-      image: '/assets/images/bg.png',
-      category: 'Academic Project',
-      featured: true,
-      label: ' ',
-      // tslint:disable-next-line:max-line-length
-      description: ' '
-    },
-    {
-      id: '1',
-      name: 'Miles',
-      image: '/assets/images/Picture1.png',
-      category: 'Academic Project',
-      featured: false,
-      label: '',
-      description: ' '
-    },
-    {
-      id: '2',
-      name: 'Face Recognition',
-      image: '/assets/images/facial-recognition.jpg',
-      category: 'Academic Project',
-      featured: false,
-      label: ' ',
-      description: ' '
-    },
-    {
-      id: '3',
-      name: 'Portfolio Website',
-      image: '/assets/images/123.jpg',
-      category: 'Academic Project',
-      featured: false,
-      label: '',
-      description: ' '
-    }
-   ];
-
+    projects = PROJECTS;
+  
+    selectedProject = PROJECTS[0];
+  
   constructor() { }
 
   ngOnInit() {
